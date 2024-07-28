ee81012f5ebf74338855140ede36256a31ab8f1b
Viraj <viraj.sonawane22@pccoepune.org>
2024-07-28 20:25:41 +0530

    No hard Coding of users direct roles from the db

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,40 +1,45 @@
 package com.luv2code.springboot.cruddemo.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig {
 
+	// add support for Jdbc .. no more hardcoded users
 	@Bean
-	public InMemoryUserDetailsManager userDetailsManager() {
-		UserDetails viraj = User.builder().username("viraj").password(passwordEncoder().encode("sjpv@5555"))
-				.roles("EMPLOYEE", "MANAGER", "ADMIN").build();
-
-		UserDetails piyush = User.builder().username("piyush").password(passwordEncoder().encode("piyush@1234"))
-				.roles("EMPLOYEE", "MANAGER").build();
-
-		UserDetails john = User.builder().username("john").password(passwordEncoder().encode("john@1234"))
-				.roles("EMPLOYEE").build();
-
-		return new InMemoryUserDetailsManager(viraj, piyush, john);
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
 	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+//	@Bean
+//	public InMemoryUserDetailsManager userDetailsManager() {
+//		UserDetails viraj = User.builder().username("viraj").password(passwordEncoder().encode("sjpv@5555"))
+//				.roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+//
+//		UserDetails piyush = User.builder().username("piyush").password(passwordEncoder().encode("piyush@1234"))
+//				.roles("EMPLOYEE", "MANAGER").build();
+//
+//		UserDetails john = User.builder().username("john").password(passwordEncoder().encode("john@1234"))
+//				.roles("EMPLOYEE").build();
+//
+//		return new InMemoryUserDetailsManager(viraj, piyush, john);
+//	}
+
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
